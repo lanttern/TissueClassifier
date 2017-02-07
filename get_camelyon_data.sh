c054bd3831a9c971792bd84d47a215697c4755c1
Peyton Rose <pwrose21@gmail.com>
2017-02-07 13:40:33 -0800

    change of get data script

--- get_camelyon_data.sh
+++ get_camelyon_data.sh
@@ -4,10 +4,10 @@ wget https://s3.amazonaws.com/histowiz-data/tmp/normal.zip
 unzip metastasis.zip
 unzip normal.zip
 
-rm metastasis.zip
-rm normal.zip
+#rm metastasis.zip
+#rm normal.zip
 
 mv metastasis/ data/slide_data/camelyon_metastatic
 mv normal/ data/slide_data/camelyon_normal
 
-rm -r __MACOSX/
+#rm -r __MACOSX/
